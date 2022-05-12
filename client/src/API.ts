0fbcefd28d57a36a884976047c56c5875b9af429
didactic-goggles <58259532+didactic-goggles@users.noreply.github.com>
2022-05-12 21:39:36 +0300

    client file download

--- client/src/API.ts
+++ client/src/API.ts
@@ -1,13 +1,14 @@
 import axios, { AxiosResponse } from "axios"
 const baseUrl: string = "http://localhost:3000"
 
-export const getTransactions = async (query: {} | undefined): Promise<
-  AxiosResponse<ApiDataType>
-> => {
+export const getTransactions = async (
+  query: {} | undefined
+): Promise<AxiosResponse<ApiDataType>> => {
   try {
     const transactions: AxiosResponse<ApiDataType> = await axios.get(
-      baseUrl + "/transactions", {
-        params: query
+      baseUrl + "/transactions",
+      {
+        params: query,
       }
     )
     return transactions
@@ -51,10 +52,25 @@ export const deleteTransaction = async (
   transactionId: string
 ): Promise<string> => {
   try {
-    await axios.delete(
-      baseUrl + "/transactions/" + transactionId
-    )
+    await axios.delete(baseUrl + "/transactions/" + transactionId)
     return transactionId
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message)
+    throw new Error(error as string)
+  }
+}
+
+export const downloadFile = async (formData: {
+  type: string
+}): Promise<AxiosResponse<string>> => {
+  try {
+    const file: AxiosResponse<string> = await axios.post(
+      baseUrl + "/download",
+      formData, {
+        responseType: 'blob'
+      }
+    )
+    return file
   } catch (error) {
     if (error instanceof Error) throw new Error(error.message)
     throw new Error(error as string)
--- client/src/features/transactions/filter/Export.tsx
+++ client/src/features/transactions/filter/Export.tsx
@@ -1,12 +1,27 @@
+import fileDownload from 'js-file-download'
+import { downloadFile } from "API"
 import Dropdown from "react-bootstrap/Dropdown"
 import DropdownButton from "react-bootstrap/DropdownButton"
 
 const Export: React.FC = () => {
   const fileExtTypes = ["txt", "csv", "json", "xlsx"]
+
+  const handleExportClick = async (type: string) => {
+    const response = await downloadFile({ type })
+    fileDownload(response.data, `export.${type}`)
+  }
+
   return (
-    <DropdownButton id="dropdown-basic-button" align="end" title="Export button">
+    <DropdownButton
+      id="dropdown-basic-button"
+      align="end"
+      title="Export button"
+    >
       {fileExtTypes.map((fileType) => (
-        <Dropdown.Item key={fileType}>
+        <Dropdown.Item
+          key={fileType}
+          onClick={handleExportClick.bind(null, fileType)}
+        >
           <i className={`bi-filetype-${fileType} me-2 fs-4`}></i>
           <span className="fs-6">Export as .{fileType}</span>
         </Dropdown.Item>
